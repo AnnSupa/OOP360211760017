400bb2b7ec9a1329d19de82be38e3e75b965c091
Nantiya <Nantiyaann3345@gmail.com>
2018-05-08 15:35:26 +0700

    update data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello,My name is Nantiya");
-
+        System.out.println("Major: Information System");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
     }
 }
