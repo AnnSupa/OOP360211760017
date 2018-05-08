09d707d2bff628763925e079506c66e84c217ea9
Nantiya <Nantiyaann3345@gmail.com>
2018-05-08 15:37:44 +0700

    Delete data

--- src/oopmit/Hello.java
+++ src/oopmit/Hello.java
@@ -6,6 +6,6 @@ public class Hello {
         System.out.println("Hello,My name is Nantiya");
         System.out.println("Major: Information System");
         System.out.println("Faculty: Management Technology");
-        System.out.println("University: RMUTSV");
+
     }
 }
